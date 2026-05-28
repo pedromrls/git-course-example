e91d100c4ed3f9977025fd1ab17ee07d7870c7a1
pedromrls <pedromrls6@gmail.com>
2026-05-28 11:45:20 -0400

    Try adding the sum function to speed up the performance

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,5 +1,5 @@
 def add_numbers(a,b,c):
-    return a + b + c
+    return sum(a,b,c)
 
 # 1. extend add_numbers to allow an arbitrary number of parameters
 # 2. investigate and resolve security explout of add_numbers()
