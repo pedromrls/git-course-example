330d663a1653141b691d2df30561453b43c5e5f8
pedromrls <pedromrls6@gmail.com>
2026-05-28 11:26:16 -0400

    added suggested changes comments to add_numbers functions as well as the implementation of cal_diff in git_tutorial.py

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ we are learning how to make commits.
 1. make some changes
 2. git add -- "stage" the changes you want to commit
 3. commit the changes
+
+his
--- git_tutorial.py
+++ git_tutorial.py
@@ -1,2 +1,10 @@
 def add_numbers(a,b,c):
     return a + b + c
+
+# 1. extend add_numbers to allow an arbitrary number of parameters
+# 2. investigate and resolve security explout of add_numbers()
+# 3. make the implementation of add_numbers() more performant
+# 4. add documentation for add_number()
+
+def cal_diff(a, b):
+    return a - b 
